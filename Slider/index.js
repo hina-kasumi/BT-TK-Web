01eedf5f126bd97d90b6e7b9394c29ecf0fb4ccd
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-10-09 14:53:08 +0700

    update slider

--- Slider/index.js
+++ Slider/index.js
@@ -1,19 +1,59 @@
+"use strict";
+
 let back = document.querySelector('#prev')
 let next = document.querySelector('#next')
 
-let current = 0;
+let currTran = 0;
 let elem = document.querySelectorAll('#slider__list img');
 
-function prevImg(){
-    elem[current].removeAttribute('id');
-    current = (current > 0)?(current - 1) % elem.length:elem.length - 1;
-    elem[current].setAttribute('id', 'active');
+let inter;
+
+function startInterval() {
+    inter = setInterval(nextImg, 5000);
 }
 
-function nextImg(){
-    elem[current].removeAttribute('id');
-    current = (current + 1) % elem.length;
-    elem[current].setAttribute('id', 'active');
+startInterval(); // bắt đầu tự động chuyển ảnh
+
+// chuyển ảnh khi click vào nút prev
+function prevImg() {
+    clearInterval(inter);
+    currTran += 100;
+    if (currTran > 0)
+        currTran = -100 * (elem.length - 2);
+    for (let i = 0; i < elem.length; i++) {
+        elem[i].animate([
+            { transform: `translateX(${currTran - 100}%)` },
+            { transform: `translateX(${currTran}%)` }
+        ],
+            {
+                duration: 1000,
+                fill: 'forwards'
+            });
+    }
+    startInterval();
 }
 
-setInterval(nextImg, 3000);
+// chuyển ảnh khi click vào nút next
+function nextImg() {
+    clearInterval(inter);
+    for (let i = 0; i < elem.length; i++) {
+        elem[i].animate([
+            { transform: `translateX(${currTran}%)` },
+            { transform: `translateX(${currTran - 100}%)` }
+        ],
+            {
+                duration: 1000,
+                fill: 'forwards'
+            });
+    }
+    currTran -= 100;
+    if (currTran == -100 * (elem.length - 1)) {
+        currTran = 0;
+    }
+    startInterval();
+}
+
+back.addEventListener('click', prevImg);
+next.addEventListener('click', nextImg);
+
+
