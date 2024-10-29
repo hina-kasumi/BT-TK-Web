33f83b25f8d0b2ece18f3080c64331b6ec14bd25
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2024-10-29 17:04:46 +0700

    up form checker

--- Lab.form3/index.js
+++ Lab.form3/index.js
@@ -1,12 +1,29 @@
-function isValidEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    console.log(regex.test(email));
-    if(!regex.test(email))
-        alert('Please enter a valid email');
-}
+function valid(e) {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const phoneNumber = e.target['phone-num'].value;
+    const day = parseInt(e.target.day.value);
+    const month = parseInt(e.target.month.value);
+    const year = parseInt(e.target.year.value);
+    const date = new Date(year, month - 1, day);
+    const checker = /^[\p{L}\s]+$/u;
+    const location = /^[\p{L}\d\s]+$/u;
 
-function confirmEmail(confirm) {
-    const email = document.getElementById('email').value;
-    if (email !== confirm)
-        alert('Please enter a correct email');
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        alert("Email không hợp lệ");
+    } else if (email !== e.target['confirm-email'].value) {
+        alert("nhập lại email bạn đã nhập");
+    } else if (!/^\d{10}$/.test(phoneNumber)) {
+        // Kiểm tra số điện thoại
+        alert("Số điện thoại không hợp lệ");
+    } else if ((date.getFullYear() !== year) && (date.getMonth() !== month - 1) && (date.getDate() !== day)) {
+        alert("Ngày sinh không hợp lệ.");
+    } else if (checker.test(e.target['first_name'].value) && checker.test(e.target['last_name'].value)) {
+        alert('tên chỉ chứa chữ bình thường');
+    } else if (!location.test(e.target.location.value)
+        || !checker.test(e.target.district.value)
+        || !checker.test(e.target.city.value)
+    ) {
+        alert('tên địa chỉ không chứa kí tự đặc biệt')
+    }
 }
